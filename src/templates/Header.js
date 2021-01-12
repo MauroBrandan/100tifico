85740871e8f3aea42dc7668d17c1f1aa422cb512
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-01-11 22:02:56 -0300

    Estilos arreglados

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,11 +3,11 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">100tifi.co</a>
+                    <a href="#/">100tifi.co</a>
                 </h1>
             </div>
             <div class="Header-nav">
-                <a href="#/about/">About</a>
+                <a href="https://rickandmortyapi.com/" target="_blank">About</a>
             </div>
         </div>
     `;
