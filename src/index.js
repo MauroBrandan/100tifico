7866f39ad7ecd4a596e0f86018b836e71933c602
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-01-11 23:04:53 -0300

    Logica del buscador

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import router from './routes'
 
+let inputSearch = document.getElementById('search')
+let button = document.getElementById('search-button')
+
+button.addEventListener('click', idSearch)
+
+function idSearch (){
+    let search = inputSearch.value
+
+    if (parseInt(search) > 671 || parseInt(search) <= 0){
+        alert('Solo hay 671 personajes, empezando por el 1')
+    }
+
+    window.location.hash = `/${search}`
+}
+
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router)
