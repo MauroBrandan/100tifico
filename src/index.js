6eda83f24e68a2439f9a0237e0a7f888453db406
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-01-12 00:55:27 -0300

    Estilos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import router from './routes'
 import IdSearch from './utils/IdSearch'
+import getCharactersNumber from './utils/getCharactersNumber'
 
 let inputSearch = document.getElementById('search')
 let button = document.getElementById('search-button')
+let p = document.getElementById('search-p')
+const count = getCharactersNumber()
+
+count
+    .then(count => p.innerHTML = `Para buscar un personaje en especifico ingrese un ID. Puede ingresar desde 1 hasta ${count}.`)
+    .catch(error => console.error('Fetch Error', error))
 
 button.addEventListener('click', () => IdSearch(inputSearch.value))
 
