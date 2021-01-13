da2a71f3732129f724b4ad30e784e2014f975e4d
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-01-12 21:12:35 -0300

    Footer

---
+++ src/templates/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    const view = `
+        <div class="Footer-main">
+            <p>
+                Desarrollado por <a href="https://maurobrandan.com/" target="_blank">Mauro Brandan</a> gracias a la API de <a href="https://rickandmortyapi.com/" target="_blank">Rick and Morty</a> y el curso de <a href="https://platzi.com/cursos/spa-javascript/" target="_blank">Platzi</a>
+            </p>
+        </div>
+    `;
+    return view
+}
+
+export default Footer
